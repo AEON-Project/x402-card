50ba8aa814f2c642a6b2c7ee762413f7ad15e7f4
袁勇 <yuanyong19891989@gmail.com>
2026-04-02 17:04:13 +0800

    init

---
+++ src/constants.mjs
@@ -0,0 +1,5 @@
+export const MIN_AMOUNT = 0.6;
+export const MAX_AMOUNT = 800;
+export const POLL_INTERVAL = 5000;
+export const MAX_POLLS = 10;
+export const BSC_RPC_URL = "https://few-boldest-spring.bsc.quiknode.pro/ec468d8a1ea2c310457b2e2f4eea257e62ba3b1e/";
